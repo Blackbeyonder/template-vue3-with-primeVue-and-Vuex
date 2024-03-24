eabb222bd73f55d5d13bdd8e4274dbe067d72582
Blackbeyonder <54973451+Blackbeyonder@users.noreply.github.com>
2024-03-24 12:39:59 -0600

    moved home to views folder

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 // import HomeView from "../views/HomeView.vue";
-import home from "../components/homeComponent/homeTemplate.vue";
+import home from "../views/homeView/homeTemplate.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
