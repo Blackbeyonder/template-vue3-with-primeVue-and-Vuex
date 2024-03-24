cd9cc9c94a0858743ac0aceb2d4e804fd394eac0
Blackbeyonder <54973451+Blackbeyonder@users.noreply.github.com>
2024-03-24 17:44:02 -0600

    work but i will divide in module

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,61 @@
-import { createStore } from "vuex";
-import userModule from './modules/user';
+import { createStore, Store } from 'vuex';
 
+// Define el tipo de estado para el usuario
+interface UserState {
+  user: User | null;
+}
 
-export default createStore({
-  modules: {
-    user: userModule
+// Define el tipo para el objeto de usuario
+interface User {
+  username: string;
+  email: string;
+  // Otros campos según sea necesario
+}
+
+// Inicializa el estado del usuario
+const initialState: UserState = {
+  user: null
+};
+
+export default createStore<UserState>({
+  state: initialState,
+  mutations: {
+    // Mutación para establecer el usuario
+    setUser(state, user: User) {
+      state.user = user;
+    },
+    // Mutación para limpiar el usuario (cerrar sesión)
+    clearUser(state) {
+      state.user = null;
+    }
   },
+  actions: {
+    // Acción para iniciar sesión
+    async login({ commit }, infoUser) {
+      try {
+        
+        commit('setUser', infoUser);
+      } catch (error) {
+        // Manejo de errores
+        console.error('Error en el inicio de sesión:', error);
+        throw error; // Opcional: lanza el error para que el componente pueda manejarlo
+      }
+    },
+    // Acción para cerrar sesión
+    async logout({ commit }) {
+      try {
+        // Aquí realizarías la lógica para cerrar sesión, como limpiar tokens de sesión, etc.
+        // Llama a la mutación clearUser para limpiar el usuario del estado
+        commit('clearUser');
+      } catch (error) {
+        // Manejo de errores
+        console.error('Error al cerrar sesión:', error);
+        throw error; // Opcional: lanza el error para que el componente pueda manejarlo
+      }
+    }
+  },
+  getters: {
+    // Getter para obtener el usuario actual
+    currentUser: state => state.user
+  }
 });
