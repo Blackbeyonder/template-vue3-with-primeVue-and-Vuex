433b4b8b9bb658cb58aa16acaeb82b85f2eb904c
Blackbeyonder <54973451+Blackbeyonder@users.noreply.github.com>
2024-04-10 21:16:24 -0600

    modificated: formsTemplate - validator reactive with vuelidate

--- src/views/formsView/formsScript.ts
+++ src/views/formsView/formsScript.ts
@@ -1,20 +1,40 @@
 import { Options, Vue } from "vue-class-component";
 import headerComponent from "../../components/headerComponent/headerTemplate.vue";
 import footerComponent from "../../components/footerComponent/footerTemplate.vue";
+import { useVuelidate } from '@vuelidate/core'
+import { required, email } from '@vuelidate/validators'
+import { reactive } from "vue";
 
 @Options({
-  components: {
-    headerComponent,
-    footerComponent
-  }
+    components: {
+        headerComponent,
+        footerComponent
+    }
 })
 export default class formsComponent extends Vue {
+    state = reactive({
+        firstName: '',
+        lastName: '',
+    })
+    rules = {
+        firstName: { required }, // Matches state.firstName
+        lastName: { required }, // Matches state.lastName
+    }
 
-  username= "";
-  password = "";
-  objInStore={};
-  
-    
-  
+
+
+    v$: any = useVuelidate(this.rules, this.state);
+
+
+    async submitForm() {
+        const valid = await this.v$.$validate();
+
+        if (valid) {
+            console.log("Formulario válido");
+        } else {
+            console.log("Formulario inválido");
+        }
+    }
+
 
 }
--- src/views/formsView/formsTemplate.vue
+++ src/views/formsView/formsTemplate.vue
@@ -2,7 +2,24 @@
   <headerComponent />
   <div class="formsClass">
     <h1>template forms</h1>
-   
+
+    <form @submit.prevent="submitForm">
+    <div >
+      <label for="firstName">Nombre:</label>
+      <input type="text" id="firstName" v-model.trim="state.firstName">
+      <div class="text-red-500">
+        <span v-if="!v$.$pending && v$?.firstName?.$error">*valor requerido</span>
+      </div>
+    </div>
+    <div>
+      <label for="lastName">Apellido:</label>
+      <input type="text" id="lastName" v-model.trim="state.lastName">
+      <div class="text-red-500">
+        <span v-if="!v$.$pending && v$?.lastName?.$error">*valor requerido</span>
+      </div>
+    </div>
+    <button type="submit">Enviar</button>
+  </form>
   </div>
 
   <footerComponent></footerComponent>
